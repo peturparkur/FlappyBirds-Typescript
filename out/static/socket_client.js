cd08e338f9133c3ad6b542231f70da392e0ee001
peturparkur <61064454+peturparkur@users.noreply.github.com>
2021-06-30 12:45:24 +0100

    Update socket_client.js

--- out/static/socket_client.js
+++ out/static/socket_client.js
@@ -1,4 +1,4 @@
-const PORT : number = 4040;
+const PORT = 4040;
 //const PORT = 8080; //for docker?
 const client = new WebSocket("ws://ec2-18-133-247-153.eu-west-2.compute.amazonaws.com:" + PORT); //connect to server
 let clientId = null;
